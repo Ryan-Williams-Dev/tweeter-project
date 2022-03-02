28d0a21433c0465e36b4641faec68c3f0689cb01
Ryan Williams <r.d.williams.ca@gmail.com>
2022-03-02 03:37:04 +0000

    day 2 implementations, implemented the character counter, created the tweet display format, and added a few hover effects

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('#tweet-text').keyup(function() {
+    const tweetLength = $(this).val().length;
+    const counter = $(this).parents().find(".counter")
+    counter.val(140 - tweetLength);
+    if (counter.val() < 0) {
+      $(counter).attr('id', 'neg-count');
+    } else {
+      $(counter).attr('id', '');
+    }
+  });
+
+});
