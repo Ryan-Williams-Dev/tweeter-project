37f9dbbc92de9c4fc6fc656539abda9a10e49c02
Ryan Williams <r.d.williams.ca@gmail.com>
2022-03-03 05:36:32 +0000

    implemented a button in the bottom corner to take back to top of page, and get the tweet prompt from the header to dissapear when down the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -77,6 +77,41 @@ $(document).ready(function () {
     return div.innerHTML;
   };
 
+  $('#new-tweet-reveal').on('click', () => {
+    if ($('.new-tweet').is(':visible')) {
+      $('.new-tweet').slideUp();
+      $('#error-msg').slideUp();
+    } else {
+      $('.new-tweet').slideDown();
+      $('#tweet-text').focus();
+    }
+  });
+
+  $(".scroll-up-button").hover(
+    function() {
+      $(this).addClass('fa-bounce');
+    },
+    function() {
+      $(this).removeClass('fa-bounce');
+    }
+  )
+
+  $(".scroll-up-button").on('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  })
+
+  $('.scroll-up-button').slideUp();
+
+  $(window).on('scroll', () => {
+    if ($(window).scrollTop() === 0) {
+      $('.newTweetPrompt').slideDown();
+      $('.scroll-up-button').slideUp();
+    } else {
+      $('.newTweetPrompt').slideUp();
+      $('.scroll-up-button').slideDown();
+    }
+  })
+
   loadTweets();
   
 });
